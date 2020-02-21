58e9803002e6215bea5af39101b854731cc8e068
William <wilham77@gmail.com>
2020-02-21 17:14:15 +1100

    Static updates

--- myvidsite/myvidsite/settings.py
+++ myvidsite/myvidsite/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+REPOSITORY_ROOT = os.path.dirname(BASE_DIR)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
